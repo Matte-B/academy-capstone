96c0164e48192fcc52c819c31f103323cf81d187
Mathias Bongaerts <mathiasbongaerts@hotmail.com>
2024-02-08 14:46:28 +0000

    Cleaned the data

--- project/pipeline/clean_data.py
+++ project/pipeline/clean_data.py
@@ -1,11 +1,64 @@
-from fetch_data import *
-from pyspark.sql import DataFrame, SparkSession
+from fetch_data import fetch_data
+from pyspark.sql import DataFrame, SparkSession, Column
+from pyspark.sql import functions as sf
+from pyspark.sql.types import (TimestampType)
 
-def clean_frame(frame: DataFrame) -> DataFrame:
-    pass
+def extract_struct_field(frame, column, fields):
+    for field in fields:
+        frame = frame.withColumn(field, sf.col(column).getField(field))
+    
+    dropped_struct_frame = frame.drop(column)
+    return dropped_struct_frame
 
+def flatten_frame(frame):
+    structs = {
+        "coordinates" : ["latitude", "longitude"],
+        "date" : ["local", "utc"]
+    }
 
-if __name__ == "__main__":
+    for struct_column, fields in structs.items():
+        frame = extract_struct_field(frame, struct_column, fields)
+    
+    return frame
+
+def drop_columns(frame):
+    drops = ["local", "locationId"]
+    return frame.drop(*drops)
+
+def rename_columns(frame):
+    renames = {
+        "isAnalysis" : "is_analysis",
+        "isMobile" : "is_mobile",
+        "utc" : "timestamp_utc"
+    }
+
+    for old_name, new_name in renames.items():
+        frame = frame.withColumnRenamed(old_name, new_name)
+
+    return frame
+
+def cast_columns(frame):
+    mapping ={
+        TimestampType: ["timestamp_utc"]
+    }
+
+    for datatype, colnames in mapping.items():
+        for colname in colnames:
+            frame = frame.withColumn(
+                colname, sf.col(colname).cast(datatype())
+            )
+
+    return frame
+
+def clean_frame():
     frame = fetch_data()
-    frame.show(5)
-    frame.printSchema()
+
+    for transformation in (
+        flatten_frame,
+        drop_columns,
+        rename_columns,
+        cast_columns
+    ):
+        frame = transformation(frame)
+    
+    return frame
