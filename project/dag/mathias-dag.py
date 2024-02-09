94963e997054d5971414f3f03e585f268cc9f111
Mathias Bongaerts <mathiasbongaerts@hotmail.com>
2024-02-09 09:48:40 +0000

    Created DAG for airflow

---
+++ project/dag/mathias-dag.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.batch import BatchOperator
+from pendulum import datetime
+
+TASK = "submit_mathias_capstone_job"
+JOB = "Mathias-capstone-job"
+QUEUE = "academy-capstone-winter-2024-job-queue"
+DEF = "Mathias-capstone-batch"
+
+START = datetime(year=2024, month=2, day=9, tz="Europe/Belgium")
+
+with DAG(
+    dag_id="run_container",
+    description="Run container that executes pipeline",
+    default_args={"owner": "Mathias"},
+    schedule_interval="0 0 9 2 *",
+    start_date=START,
+) as dag:
+    submit_batch_job = BatchOperator(
+    task_id=TASK
+    job_name=JOB,
+    job_queue=QUEUE,
+    job_definition=DEF,
+)
